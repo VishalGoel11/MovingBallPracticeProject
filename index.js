0da36792607384598676e51389f2279d6bba7769
Vishal Goel <vishugoel2812@gmail.com>
2022-10-25 03:43:01 +0530

    ball UI changes

--- index.js
+++ index.js
@@ -1,54 +1,79 @@
 var ball=document.getElementById("ball");
-ball.style.marginTop="10px";
-ball.style.marginLeft="15px";
-// ball.style.marginRight="px";
-// ball.style.marginBottom="342px";
-var a=2;
-var b=2;
-var c=2;
-var d=2;
-var interval1;
-var interval2;
-var interval4;
-var interval5;
+var a=1;
+var b=1;
+var c=1;
+var d=1;
+var interval1=true;
+var interval2=true;
+var interval4=true;
+var interval5=true;
 document.getElementById("btn1").addEventListener("click",()=>{
-interval1=setInterval(function(){
+     interval2=false;
+     interval4=false;
+     interval5=false;
+     // while(interval1==true){
      let ball2=ball.style.marginTop;
+     console.log(ball2);
      let ball1=parseInt(ball2.substring(0,ball2.length-2));
      ball1-=a;
      ball.style.marginTop=ball1+"px";
      a++;
-},200);})
+     console.log(ball1);
+     // }
+     //  interval1=true;
+     //  interval2=true;
+     //  interval4=true;
+     //  interval5=true;
+      
+})
 document.getElementById("btn2").addEventListener("click",()=>{
- interval2=setInterval(function(){
+     interval1=false;
+     interval4=false;
+     interval5=false;
+     // while(interval2==true){
      let ball2=ball.style.marginLeft;
      let ball1=parseInt(ball2.substring(0,ball2.length-2));
-     ball1-=b;
+     ball1-=c;
      ball.style.marginLeft=ball1+"px";
-     b++;
-},200);
+     c++;
+     console.log(ball1);
+     // }
+     // interval1=true;
+     //  interval2=true;
+     //  interval4=true;
+     //  interval5=true;
 })
 document.getElementById("btn5").addEventListener("click",()=>{
-     interval5= setInterval(function(){
+     interval2=false;
+     interval1=false;
+     interval5=false;
+     // while(interval4==true){
           let ball2=ball.style.marginTop;
           let ball1=parseInt(ball2.substring(0,ball2.length-2));
-          ball1+=a;
+          ball1+=d;
           ball.style.marginTop=ball1+"px";
-          a++;
-     },200);
+          d++;
+          console.log(ball1);
+     // }
+     // interval1=true;
+     //  interval2=true;
+     //  interval4=true;
+     //  interval5=true;
 });
 document.getElementById("btn4").addEventListener("click",()=>{
-       interval4=setInterval(function(){
+     interval2=false;
+     interval4=false;
+     interval1=false;
+     // while(interval5==true){
           let ball2=ball.style.marginLeft;
           let ball1=parseInt(ball2.substring(0,ball2.length-2));
           ball1+=b;
           ball.style.marginLeft=ball1+"px";
-          a++;
-     },200);
-});
-document.getElementById("btn3").addEventListener("click",()=>{
-     clearInterval(interval1);
-     clearInterval(interval2);
-     clearInterval(interval4);
-     clearInterval(interval5);
+          b++;
+          console.log(ball1);
+     // }
+     // interval1=true;
+     //  interval2=true;
+     //  interval4=true;
+     //  interval5=true;
 });
